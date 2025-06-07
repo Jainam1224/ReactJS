99880c632d9084887d6e1dcdb1174e7799145518
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-08 00:20:21 +0530

    sold out logic and style added

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -116,11 +116,11 @@ function Menu() {
 
 function Pizza({ pizzaObj }) {
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <h3>{pizzaObj.name}</h3>
       <p>{pizzaObj.ingredients}</p>
-      <span>{pizzaObj.price ? `$${pizzaObj.price}` : "SOLD OUT"}</span>
+      <span>{pizzaObj.soldOut ? "SOLD OUT" : `$${pizzaObj.price}`}</span>
     </li>
   );
 }
