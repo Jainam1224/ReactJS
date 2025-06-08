502ad79a93dfb0aec7ef22861479dcc64e4bcca3
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-08 19:26:41 +0530

    added additional props so we can ship this as a package and can be reusable

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={5} onSetRating={setMovieRating} />
+      <p>The movie is rated as: {movieRating}</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
