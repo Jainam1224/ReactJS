2b668967973787088c83ec1dea9375468b23a156
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-14 14:52:54 +0530

    setup of react router

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <div>Worldwise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        {/* The * path matches for the route which didn't match to any other route - PageNotFound */}
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
