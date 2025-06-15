dfaf4032b02b858bbcfb165a60ebbd330fd8858d
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-15 16:18:22 +0530

    json server for cities data and implementing citylist component

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,18 +1,55 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      setIsLoading(true);
+
+      try {
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        console.log("data", data);
+        setCities(data);
+      } catch (err) {
+        console.error("Fetch error:", err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          {/* Index route is the default route to be renderend when we land to /app page */}
+          <Route index element={<Navigate replace to="cities" />} />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="countries" element={<p>{"countries"}</p>} />
+          <Route path="form" element={<p>{"form"}</p>} />
+        </Route>
         {/* The * path matches for the route which didn't match to any other route - PageNotFound */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
