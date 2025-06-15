d5eab85a2092e0444495f869f4a7f1531965e532
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-15 14:48:23 +0530

    sidebar and map layout created

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -3,6 +3,7 @@ import Product from "./pages/Product";
 import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
         {/* The * path matches for the route which didn't match to any other route - PageNotFound */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
