0a3f826f37e2e20d50b3e702896f3436a2d690ac
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-22 02:35:09 +0530

    creating action creators for each dispatch

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -36,18 +36,46 @@ function accountReducer(state = initialStateAccount, action) {
 
 const store = createStore(accountReducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-console.log(store.getState());
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// console.log(store.getState());
 
-store.dispatch({ type: "account/withdraw", payload: 200 });
-console.log(store.getState());
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
-console.log(store.getState());
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
+// console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
-console.log(store.getState());
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
 
 // const ACOOUNT_DEPOSIT = "account/deposit";
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+console.log(store.getState());
+
+store.dispatch(requestLoan(1000, "Buy a cheap car"));
+console.log(store.getState());
+store.dispatch(payLoan());
+console.log(store.getState());
