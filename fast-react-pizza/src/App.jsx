8a90a7f42ec335af753ea61e7f3a78d401f30eea
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-22 17:53:51 +0530

    create new order using react router dom Form component - new way

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -4,7 +4,9 @@ import Home from "./ui-common/Home.jsx";
 import Error from "./ui-common/Error.jsx";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui-common/AppLayout.jsx";
 
@@ -29,6 +31,7 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
         path: "/order/:orderId",
