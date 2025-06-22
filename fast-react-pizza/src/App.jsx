887e4647035b6d2792fbf8f565cbc140262e8ff1
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-22 16:38:20 +0530

    creating router using createBrowserRouter - new way

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-const App = () => {
-  return <div>App</div>;
-};
+import Home from "./ui-common/Home.jsx";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+// This helps more with data loaders, data actions or data features we need to use it in this way
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  { path: "/cart", element: <Cart /> },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
 
 export default App;
