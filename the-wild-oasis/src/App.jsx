133138f2ebbb6db439e6154d9d8b923e071456f2
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-23 00:12:10 +0530

    example of creating styled component

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <H1>The Jainam Sanghvi</H1>
+    </div>
+  );
 };
 
 export default App;
