cdff5d290c4d464882a3c2a9328fe53b57a60fe7
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-22 18:39:51 +0530

    added tailwind css to all the components

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,16 +1,15 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import Home from "./ui-common/Home.jsx";
-import Error from "./ui-common/Error.jsx";
+import Home from "./ui-common/Home";
+import Error from "./ui-common/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
-import AppLayout from "./ui-common/AppLayout.jsx";
+import AppLayout from "./ui-common/AppLayout";
 
-// This helps more with data loaders, data actions or data features we need to use it in this way
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
